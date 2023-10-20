0641dc4c731678f1d77df36bd94682ccb441c63c
Atkinss1 <jordanatkins@live.com>
2023-10-20 17:59:37 -0600

    move: API request into fetchBreedDescription function body

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,19 @@
 const request = require('request');
-const url = process.argv.slice(2);
-const API = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const catSearch = API.concat(url);
 
-request(catSearch, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log("There was an error with the API: ", error);
-  }
-  data.length === 0 ? console.log('The details on that cat breed is unavailable.') : console.log(data[0].description);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  
+  const API = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  const catSearch = API.concat(breedName);
+  
+  request(catSearch, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      console.log(null, error);
+    }
+    data.length === 0 ? console.log(null) : console.log(data[0].description);
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
