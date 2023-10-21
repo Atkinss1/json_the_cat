bf23f0b2dc82e5ab9b9d37fc8acd0abf6e8ec38f
Atkinss1 <jordanatkins@live.com>
2023-10-20 19:19:05 -0600

    refactor: if error return undefined

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,19 @@
 const request = require('request');
-
 const fetchBreedDescription = (breedName, callback) => {
   
   const API = 'https://api.thecatapi.com/v1/breeds/search?q=';
   const catSearch = API.concat(breedName);
   
-  request(catSearch, (error, response, body) => {
+  request(catSearch, (error, _response, body) => {
     const data = JSON.parse(body);
-    if (error) {
-      console.log(null, error);
+    if (error || data.length === 0) {
+      callback(undefined);
+    } else {
+      callback(error, data[0].description);
     }
-    data.length === 0 ? console.log(null) : console.log(data[0].description);
   });
 };
+
 
 module.exports = {
   fetchBreedDescription
