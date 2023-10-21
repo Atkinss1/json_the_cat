39cc363d938fad0468ceee6597f9d6ea50106818
Atkinss1 <jordanatkins@live.com>
2023-10-20 19:43:44 -0600

    add: response.statusCode greater than 400, line 16 error callback, line 12 error callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,10 +4,16 @@ const fetchBreedDescription = (breedName, callback) => {
   const API = 'https://api.thecatapi.com/v1/breeds/search?q=';
   const catSearch = API.concat(breedName);
   
-  request(catSearch, (error, _response, body) => {
+  request(catSearch, (error, response, body) => {
     const data = JSON.parse(body);
-    if (error || data.length === 0) {
-      callback(undefined);
+    if (response.statusCode > 400) {
+      callback(`Error: status code ${response.statusCode}`, null);
+    }
+    if (error) {
+      callback(error, null);
+    }
+    if (data.length === 0) {
+      callback(null, `Error: ${breedName} was not found`);
     } else {
       callback(null, data[0].description);
     }
